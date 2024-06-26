7b92aae77d103144c6387676000cc0a4f1ce2cee
MatisAgr <matisagr@gmail.com>
2024-06-26 16:40:22 +0200

    Add Link parameter

--- front/src/Components/Prices/Prices.jsx
+++ front/src/Components/Prices/Prices.jsx
@@ -22,6 +22,7 @@ export default function Prices() {
                         ]}
                         action="S'abonner"
                         size="small"
+                        link="/register"
                     />
 
                     <CardPrice
@@ -34,6 +35,7 @@ export default function Prices() {
                         ]}
                         action="S'abonner"
                         size="big"
+                        link="/register"
                     />
 
                     <CardPrice
@@ -46,6 +48,7 @@ export default function Prices() {
                         ]}
                         action="S'abonner"
                         size="small"
+                        link="/register"
                     />
 
                 </div>
