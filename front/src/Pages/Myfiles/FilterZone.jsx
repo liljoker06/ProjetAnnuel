2c3774aa7357cb8c161bcd7be1d684c2f64f3a1b
Tisma <matisagr@gmail.com>
2024-08-01 18:02:02 +0200

    chore: Update FilterZone component to include image filter option

--- front/src/Pages/Myfiles/FilterZone.jsx
+++ front/src/Pages/Myfiles/FilterZone.jsx
@@ -3,27 +3,30 @@ import { CSSTransition } from 'react-transition-group';
 import './SortFilterZone.css';
 
 export default function FilterZone({ isOpen, onClose, handleFilterChange, filters }) {
-  return (
-    <CSSTransition
-      in={isOpen}
-      timeout={300}
-      classNames="filter-zone"
-      unmountOnExit
-    >
-      <div className="bg-white p-4 rounded-lg shadow mb-4 text-gray-500">
-        <h3 className="text-lg font-semibold mb-4 text-gray-700">Filtrer par type de fichier</h3>
-        <div className="flex flex-col">
-          <label className="flex items-center mb-2">
-            <input type="checkbox" className="mr-2 custom-checkbox" checked={filters.doc} onChange={() => handleFilterChange('doc')} /> Documents
-          </label>
-          <label className="flex items-center mb-2">
-            <input type="checkbox" className="mr-2 custom-checkbox" checked={filters.sheet} onChange={() => handleFilterChange('sheet')} /> Tableurs
-          </label>
-          <label className="flex items-center mb-2">
-            <input type="checkbox" className="mr-2 custom-checkbox" checked={filters.slides} onChange={() => handleFilterChange('slides')} /> Présentations
-          </label>
-        </div>
-      </div>
-    </CSSTransition>
-  );
+    return (
+        <CSSTransition
+            in={isOpen}
+            timeout={300}
+            classNames="filter-zone"
+            unmountOnExit
+        >
+            <div className="bg-white p-4 rounded-lg shadow mb-4 text-gray-500">
+                <h3 className="text-lg font-semibold mb-4 text-gray-700">Filtrer par type de fichier</h3>
+                <div className="flex flex-col">
+                    <label className="flex items-center mb-2">
+                        <input type="checkbox" className="mr-2 custom-checkbox" checked={filters.doc} onChange={() => handleFilterChange('doc')} /> Documents
+                    </label>
+                    <label className="flex items-center mb-2">
+                        <input type="checkbox" className="mr-2 custom-checkbox" checked={filters.sheet} onChange={() => handleFilterChange('sheet')} /> Tableurs
+                    </label>
+                    <label className="flex items-center mb-2">
+                        <input type="checkbox" className="mr-2 custom-checkbox" checked={filters.slides} onChange={() => handleFilterChange('slides')} /> Présentations
+                    </label>
+                    <label className="flex items-center mb-2">
+                        <input type="checkbox" className="mr-2 custom-checkbox" checked={filters.image} onChange={() => handleFilterChange('image')} /> Images
+                    </label>
+                </div>
+            </div>
+        </CSSTransition>
+    );
 }
