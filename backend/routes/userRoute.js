acc0144ab7c19454b7002bbf66f671dde4952044
liljoker06 <lahrimialadin@gmail.com>
2024-08-21 20:14:22 +0200

    chore: Update npm dependencies and add express-validator package

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/controllerUser');
+const validateUser = require('../middleware/validateUser');
+
 
 router.get('/', userController.getAllUsers);
-router.post('/', userController.createUser);
+router.post('/', validateUser, userController.createUser);
 
 
 module.exports = router;
