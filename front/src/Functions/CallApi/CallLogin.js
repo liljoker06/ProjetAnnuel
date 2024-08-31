30f83dbc3d6c61f77d8582881c74e03a1e3970af
Tisma <matisagr@gmail.com>
2024-08-31 21:38:46 +0200

    clean callapi

--- front/src/Functions/CallApi/CallLogin.js
+++ front/src/Functions/CallApi/CallLogin.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // connexion de l'utilisateur
 export const loginUser = async (data) => {
-    console.log('• callapi login user', 'white');
     try {
         const response = await axios.post('http://localhost:5555/api/users/login', data);
 
