cfbce4b17e1bfc865f670b02b983a8624e162119
JulienBNT <julien.binet91@gmail.com>
2024-09-02 11:05:05 +0200

    adminjsx update

--- front/src/Pages/Admin/Admin.jsx
+++ front/src/Pages/Admin/Admin.jsx
@@ -3,40 +3,56 @@ import axios from 'axios';
 
 const Admin = () => {
     const [users, setUsers] = useState([]);
+    const [showUsers, setShowUsers] = useState(false);
 
     useEffect(() => {
-        fetchUsers();
+        axios.get('http://localhost:5555/api/users')
+            .then(response => {
+                console.log('Utilisateurs récupérés:', response.data);
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des utilisateurs:', error);
+            });
     }, []);
 
-    const fetchUsers = async () => {
-        try {
-            const response = await axios.get('/api/admin/adminUsers');
-            setUsers(response.data);
-        } catch (error) {
-            console.error(error);
-        }
+    const deleteUser = (userId) => {
+        axios.delete(`http://localhost:5555/api/users/${userId}`)
+            .then(() => {
+                setUsers(users.filter(user => user.user_id !== userId));
+                console.log(`Utilisateur ${userId} supprimé`);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la suppression de l\'utilisateur:', error);
+            });
     };
 
-    // const deleteUser = async (userId) => {
-    //     try {
-    //         await axios.delete(`/api/admin/users/${userId}`);
-    //         fetchUsers();
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+    const toggleUserList = () => {
+        setShowUsers(!showUsers);
+        console.log('État showUsers:', !showUsers);
+    };
 
     return (
         <div>
-            <h1>Admin Page</h1>
-            <ul>
-                {users.map((user) => (
-                    <li key={user.id}>
-                        {user.name}
-                        {/* <button onClick={() => deleteUser(user.id)}>Delete</button> */}
-                    </li>
-                ))}
-            </ul>
+            <h1>Admin</h1>
+            <button onClick={toggleUserList} style={{marginTop: 100}}>
+                Liste des utilisateurs
+            </button>
+            {showUsers && (
+                <ul>
+                    {users.map(user => (
+                        <>
+                        <li key={user.user_id}>
+                            {user.user_fname} - {user.user_lname}
+                            <button onClick={() => deleteUser(user.user_id)} style={{ marginLeft: 10 }}>
+                                Supprimer utilisateur
+                            </button>
+                        </li>
+                        
+                        </>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
